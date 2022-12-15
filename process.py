7e316cf1cb8e4027a6d5b00b1776c3f0578a2729
Tejas Santosh Tamkar <72966382+tejastamkar@users.noreply.github.com>
2022-12-15 08:40:22 +0530

    added protoc

--- process.py
+++ process.py
@@ -33,11 +33,10 @@ def ImagePath(path):
     bounding_boxes = []
     confidences = []
     class_numbers = []
-
     image_input = cv2.imread(path)
     blob = cv2.dnn.blobFromImage(
         image_input, 1/255.0, (416, 416), swapRB=True, crop=False)
-    blob_to_show = blob[0, :, :, :].transpose(1, 2, 0)
+    # blob_to_show = blob[0, :, :, :].transpose(1, 2, 0)
     network.setInput(blob)
     output_from_network = network.forward(layers_names_output)
     h, w = image_input.shape[:2]
@@ -56,11 +55,6 @@ def ImagePath(path):
                     [x_min, y_min, int(box_width), int(box_height)])
                 confidences.append(float(confidence_current))
                 class_numbers.append(class_current)
-
-        # %matplotlib inline
-        # plt.rcParams['figure.figsize'] = (5.0,5.0)
-        # plt.imshow(cv2.cvtColor(image_input, cv2.COLOR_BGR2RGB))
-        # plt.show()
 
         for item in sorted(set(class_numbers)):
             OutputDec = labels[item]
